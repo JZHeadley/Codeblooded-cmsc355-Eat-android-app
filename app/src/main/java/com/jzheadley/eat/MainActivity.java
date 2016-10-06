e0440b9c944fa18197e8dd58a2cf67b86a0cfb64
Tom <tomash73@gmail.com>
2016-10-06 18:04:20 -0400

    description activity

--- app/src/main/java/com/jzheadley/eat/MainActivity.java
+++ app/src/main/java/com/jzheadley/eat/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.restaurant_description);
+//        setContentView(R.layout.activity_main);
 
     }
 
