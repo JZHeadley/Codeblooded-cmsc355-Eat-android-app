6bf10959a18ca1eba4bb895c524b40c92472d599
Tom <hi@tsakal.com>
2016-10-10 22:30:53 -0400

    done button opening hours

--- app/src/main/java/com/jzheadley/eat/views/MenuHoursActivity.java
+++ app/src/main/java/com/jzheadley/eat/views/MenuHoursActivity.java
@@ -1,7 +1,9 @@
 package com.jzheadley.eat.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.jzheadley.eat.R;
 
@@ -13,5 +15,10 @@ public class MenuHoursActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.opening_hours);
+    }
+
+    public void onDoneButtonClick(View view) {
+        Intent doneIntent = new Intent(view.getContext(), RestaurantCreationActivity.class);
+        view.getContext().startActivity(doneIntent);
     }
 }
