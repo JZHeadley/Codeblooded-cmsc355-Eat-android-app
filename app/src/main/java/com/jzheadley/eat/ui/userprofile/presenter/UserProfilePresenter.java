cc959d1570527751c86c148d31ed9c27d22f9d88
parrycj <parrycj@vcu.edu>
2016-11-07 12:25:27 -0500

    UserProfile page created

--- app/src/main/java/com/jzheadley/eat/ui/userprofile/presenter/UserProfilePresenter.java
+++ app/src/main/java/com/jzheadley/eat/ui/userprofile/presenter/UserProfilePresenter.java
@@ -1,13 +1,48 @@
 package com.jzheadley.eat.ui.userprofile.presenter;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.jzheadley.eat.data.models.User;
+import com.jzheadley.eat.data.services.UserService;
 import com.jzheadley.eat.ui.base.presenter.BasePresenterImpl;
 import com.jzheadley.eat.ui.userprofile.view.UserProfileActivity;
 
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class UserProfilePresenter extends BasePresenterImpl {
+    private static final String TAG = "UserProfilePresenter";
+    private UserService userService;
     private UserProfileActivity userProfileActivity;
 
-    public UserProfilePresenter(UserProfileActivity userProfileActivity) {
+    public UserProfilePresenter(UserProfileActivity userProfileActivity, UserService userService) {
         super(userProfileActivity);
         this.userProfileActivity = userProfileActivity;
+        this.userService = userService;
     }
+
+    public void getUser() {
+        String firebaseId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        userService.getUserApi()
+                .getUserByFirebaseId(firebaseId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        userProfileActivity.displayUsername(user);
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/jzheadley/eat/ui/userprofile/view/UserProfileActivity.java
+++ app/src/main/java/com/jzheadley/eat/ui/userprofile/view/UserProfileActivity.java
@@ -1,18 +1,66 @@
 package com.jzheadley.eat.ui.userprofile.view;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.jzheadley.eat.R;
+import com.jzheadley.eat.data.models.User;
+import com.jzheadley.eat.data.services.UserService;
 import com.jzheadley.eat.ui.base.view.BaseActivity;
 import com.jzheadley.eat.ui.userprofile.presenter.UserProfilePresenter;
 
-
 public class UserProfileActivity extends BaseActivity {
-    UserProfilePresenter userProfilePresenter;
+    private UserProfilePresenter userProfilePresenter;
+    private UserService userService;
+
+
+    private EditText oldEmail, newEmail, password, newPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
+    public void displayUsername(User user) {
+        ((EditText) findViewById(R.id.user_profile_et))
+                .setHint(user.getUsername());
+    }
+
+    public void displayEmail() {
+        ((EditText) findViewById(R.id.email_profile_et))
+                .setHint(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        userProfilePresenter = new UserProfilePresenter(this);
+        setContentView(R.layout.activity_profile);
+        userService = new UserService();
+        userProfilePresenter = new UserProfilePresenter(this, userService);
+        userProfilePresenter.getUser();
+        displayEmail();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 }
+
