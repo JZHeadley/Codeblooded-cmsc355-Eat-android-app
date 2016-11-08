8cf9105009b49024b120792b919ecf228a4d01c4
Mit Amin <mit.na14@gmail.com>
2016-11-08 01:02:22 -0500

    added test for registering

--- app/src/androidTest/java/com/jzheadley/eat/views/RegisterTest.java
+++ app/src/androidTest/java/com/jzheadley/eat/views/RegisterTest.java
@@ -1,21 +1,28 @@
-package com.jzheadley.eat.ui.nearbyrestaurants.view;
+package com.jzheadley.eat.views;
 
 
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.jzheadley.eat.R;
+import com.jzheadley.eat.ui.nearbyrestaurants.view.NearbyRestaurantActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
@@ -26,13 +33,13 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class wrongPassword {
+public class RegisterTest {
 
     @Rule
     public ActivityTestRule<NearbyRestaurantActivity> mActivityTestRule = new ActivityTestRule<>(NearbyRestaurantActivity.class);
 
     @Test
-    public void wrongPassword() {
+    public void registerTest() {
         ViewInteraction appCompatImageView = onView(
                 allOf(withId(R.id.slide_in_menu_icon), withContentDescription("Navigation Menu"),
                         withParent(allOf(withId(R.id.toolbar),
@@ -47,18 +54,38 @@ public class wrongPassword {
                         isDisplayed()));
         recyclerView.perform(actionOnItemAtPosition(4, click()));
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.email), isDisplayed()));
-        appCompatEditText.perform(replaceText("EspresoTestUser@gmail.com"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.password), isDisplayed()));
-        appCompatEditText2.perform(replaceText("Welcome"), closeSoftKeyboard());
-
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.btn_login), withText("Login"), isDisplayed()));
+                allOf(withId(R.id.btn_signup), withText("Not already a member? Register Here!"), isDisplayed()));
         appCompatButton.perform(click());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.sign_up_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                        0),
+                                4),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
 
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
