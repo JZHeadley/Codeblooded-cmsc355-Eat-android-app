94c4c9acbae8b949fa0e9acd35bc6438ee8ba4e8
parrycj <parrycj@vcu.edu>
2016-11-08 01:38:08 -0500

    Update UserProfileTests and BaseActivityTest

--- app/src/androidTest/java/com/jzheadley/eat/views/BaseActivityTest.java
+++ app/src/androidTest/java/com/jzheadley/eat/views/BaseActivityTest.java
@@ -1,12 +1,6 @@
 package com.jzheadley.eat.views;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import com.jzheadley.eat.R;
@@ -14,6 +8,13 @@ import com.jzheadley.eat.ui.nearbyrestaurants.view.NearbyRestaurantActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class BaseActivityTest {
@@ -24,6 +25,19 @@ public class BaseActivityTest {
     public void ensureNavigationMenuAppears() {
         onView(withId(R.id.slide_in_menu_icon)).perform(click());
         onView(withText("Home")).check(matches(isDisplayed()));
+    }
+
+
+    /*
+    Given [ I am a customer ]
+    when [ I open the navigation menu ]
+    then [ I want to choose my username and be brought to my profile ]
+     */
+    @Test
+    public void displayUserProfileTest() {
+        onView(withId(R.id.slide_in_menu_icon)).perform(click());
+        onView(withId(R.id.material_drawer_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.user_profile_et)).check(matches(isDisplayed()));
     }
 
 
--- app/src/androidTest/java/com/jzheadley/eat/views/UserProfileTest.java
+++ app/src/androidTest/java/com/jzheadley/eat/views/UserProfileTest.java
@@ -15,19 +15,44 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class UserProfileTest {
     @Rule
     public ActivityTestRule<UserProfileActivity> mActivityTestRule = new ActivityTestRule<>(UserProfileActivity.class, false, true);
 
+
+    /*
+    Given [ I am a user ]
+    when [ I submit changes to my username and password]
+    then [ I can cancel and leave things the same ]
+    */
     @Test
-    public void restaurantDetailsConnectionTest() {
+    public void cancelUpdateTest() {
         onView(withId(R.id.user_profile_et)).check(matches(isDisplayed()));
         onView(withId(R.id.user_profile_et)).perform(typeText("newUsername"));
-        onView(withId(R.id.email_profile_et)).perform(typeText("newEmail"));
+        onView(withId(R.id.email_profile_et)).perform(typeText("newemail@gmail.com"));
         onView(withId(R.id.user_profile_update_submit_btn)).perform(click());
-        onView(withId(R.id.user_profile_et)).check(matches(withHint("newUsername")));
-        onView(withId(R.id.email_profile_et)).check(matches(withHint("newEmail")));
+        onView(withText("No")).perform(click());
+        onView(withId(R.id.user_profile_et)).check(matches(withHint("EspressoTestUser")));
+        onView(withId(R.id.email_profile_et)).check(matches(withHint("espresotestuser@gmail.com")));
     }
+
+    /*
+    Given [I am a user]
+    when [I see an error in my user profile]
+    then [I should be able to modify it to be accurate]
+     */
+    @Test
+    public void updateEmailUsernameTest() {
+        onView(withId(R.id.user_profile_et)).check(matches(isDisplayed()));
+        onView(withId(R.id.user_profile_et)).perform(typeText("newUsername"));
+        onView(withId(R.id.email_profile_et)).perform(typeText("newemail@gmail.com"));
+        onView(withId(R.id.user_profile_update_submit_btn)).perform(click());
+        onView(withText("Yes")).perform(click());
+        onView(withId(R.id.user_profile_et)).check(matches(withHint("newUsername")));
+        onView(withId(R.id.email_profile_et)).check(matches(withHint("newemail@gmail.com")));
+    }
+
 }
