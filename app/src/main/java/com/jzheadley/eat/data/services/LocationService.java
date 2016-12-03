05df893f75fbc72ec4f9561715e0d6b06277d2f1
mitVCU <mit.na14@gmail.com>
2016-12-03 18:10:23 -0500

    fixed favoriteRestaurants and update location service

--- app/src/main/java/com/jzheadley/eat/data/services/LocationService.java
+++ app/src/main/java/com/jzheadley/eat/data/services/LocationService.java
@@ -1,23 +1,58 @@
 package com.jzheadley.eat.data.services;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
-public class LocationService {
+public class LocationService extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
     private GoogleApiClient googleApiClient;
 
     public LocationService(AppCompatActivity activity) {
         googleApiClient = new GoogleApiClient.Builder(activity)
             .addApi(LocationServices.API)
             .build();
-        // LocationManager service = (LocationManager)
-        //     activity.getSystemService(Context.LOCATION_SERVICE);
-        // boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        // if (!enabled) {
-        //     Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        //     activity.startActivity(intent);
+
+
+      //  Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+/*
+        Location locationA = new Location("point A");
+
+        locationA.setLatitude(latA);
+        locationA.setLongitude(lngA);
+
+        Location locationB = new Location("point B");
+
+        locationB.setLatitude(latB);
+        locationB.setLongitude(lngB);
+
+        float distance = locationA.distanceTo(locationB); */
+    }
+    public static final String TAG = LocationService.class.getSimpleName();
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
+
+
