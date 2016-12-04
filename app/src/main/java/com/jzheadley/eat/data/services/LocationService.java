60dcc48d58dcb58b820d798da38bd98bee0d441d
mitVCU <mit.na14@gmail.com>
2016-12-03 23:11:27 -0500

    Location Service Created

--- app/src/main/java/com/jzheadley/eat/data/services/LocationService.java
+++ app/src/main/java/com/jzheadley/eat/data/services/LocationService.java
@@ -1,58 +1,159 @@
 package com.jzheadley.eat.data.services;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 
-public class LocationService extends FragmentActivity implements
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+public class LocationService extends Activity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    private static final String TAG = LocationService.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private static int UPDATE_INTERVAL = 10000;
+    private static int FATEST_INTERVAL = 5000;
+    private static int DISPLACEMENT = 10;
+    private final Context ctx;
+    private Location mLastLocation;
     private GoogleApiClient googleApiClient;
+    private boolean mRequestLocationUpdates = false;
+    private LocationRequest mLocationRequest;
 
-    public LocationService(AppCompatActivity activity) {
-        googleApiClient = new GoogleApiClient.Builder(activity)
-            .addApi(LocationServices.API)
-            .build();
-
-
-      //  Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-/*
-        Location locationA = new Location("point A");
-
-        locationA.setLatitude(latA);
-        locationA.setLongitude(lngA);
-
-        Location locationB = new Location("point B");
-
-        locationB.setLatitude(latB);
-        locationB.setLongitude(lngB);
-
-        float distance = locationA.distanceTo(locationB); */
+    public LocationService(Context ctx) {
+        this.ctx = ctx;
     }
-    public static final String TAG = LocationService.class.getSimpleName();
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (googleApiClient.isConnected() && mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+
+    public Location getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            Log.d(TAG, "getLocation: " + mLastLocation);
+            if (mLastLocation != null) {
+                return mLastLocation;
+            }
+        }
+
+
+        return null;
+    }
+
+    private void togglePeriodLocationUpdates() {
+        if (!mRequestLocationUpdates) {
+            mRequestLocationUpdates = true;
+            startLocationUpdates();
+        } else {
+            mRequestLocationUpdates = false;
+            stopLocationUpdates();
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+            return;
+
+        }
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        getLocation();
+
+        if (mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        googleApiClient.connect();
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
 
     }
 }
-
-
--- app/src/main/java/com/jzheadley/eat/ui/nearbyrestaurants/view/NearbyRestaurantActivity.java
+++ app/src/main/java/com/jzheadley/eat/ui/nearbyrestaurants/view/NearbyRestaurantActivity.java
@@ -1,13 +1,19 @@
 package com.jzheadley.eat.ui.nearbyrestaurants.view;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.jzheadley.eat.R;
 import com.jzheadley.eat.data.models.Restaurant;
+import com.jzheadley.eat.data.services.LocationService;
 import com.jzheadley.eat.data.services.RestaurantService;
 import com.jzheadley.eat.ui.adapters.RestaurantsListAdapter;
 import com.jzheadley.eat.ui.base.view.BaseActivity;
@@ -29,16 +35,20 @@ public class NearbyRestaurantActivity extends BaseActivity {
         setContentView(R.layout.activity_nearby_restaurants);
         restaurantService = new RestaurantService();
         nearbyRestaurantsPresenter = new NearbyRestaurantsPresenter(this, restaurantService);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+
         nearbyRestaurantsPresenter.showProgress();
-
-
+        Log.d(TAG, "onCreate: lajhfljkashdflkjahdsf" + (new LocationService(getBaseContext())).getLocation());
         if (nearbyRestaurantsPresenter.isNetworkAvailable()) {
             nearbyRestaurantsPresenter.loadRestaurants();
         } else {
             Snackbar.make(findViewById(R.id.toolbar), "You are disconnected from the network. "
-                    + "Please resolve your connection issues and try again.",
-                Snackbar.LENGTH_LONG).show();
+                            + "Please resolve your connection issues and try again.",
+                    Snackbar.LENGTH_LONG).show();
         }
+
     }
 
     @Override
@@ -53,12 +63,33 @@ public class NearbyRestaurantActivity extends BaseActivity {
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
-        restaurantsListAdapter = new RestaurantsListAdapter(restaurants);
+        restaurantsListAdapter = new RestaurantsListAdapter(restaurants, getBaseContext());
         recyclerView.setAdapter(restaurantsListAdapter);
 
         nearbyRestaurantsPresenter.hideProgress();
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
 
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied to access location", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
