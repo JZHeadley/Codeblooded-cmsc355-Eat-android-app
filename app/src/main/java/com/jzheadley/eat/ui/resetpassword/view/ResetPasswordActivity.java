d992cc5dfc63590f38ce10622a5cd61091a61cfd
jzheadley <zephyrinkg@gmail.com>
2016-12-09 10:31:32 -0500

    Error checked some text fields and set error messages on incorrect input

--- app/src/main/java/com/jzheadley/eat/ui/resetpassword/view/ResetPasswordActivity.java
+++ app/src/main/java/com/jzheadley/eat/ui/resetpassword/view/ResetPasswordActivity.java
@@ -1,8 +1,11 @@
 package com.jzheadley.eat.ui.resetpassword.view;
 
+import static com.jzheadley.eat.utils.Utilities.isValidEmail;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,6 +18,7 @@ import com.jzheadley.eat.ui.resetpassword.presenter.ResetPasswordPresenter;
 public class ResetPasswordActivity extends BaseActivity implements View.OnClickListener {
     private ResetPasswordPresenter resetPasswordPresenter;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +30,18 @@ public class ResetPasswordActivity extends BaseActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+
             case R.id.btn_forgot_password:
-                resetPasswordPresenter.resetPassword(
-                    ((EditText) findViewById(R.id.reset_email)).getText().toString().trim());
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                EditText newPasswordEditText = (EditText) findViewById(R.id.reset_email);
+                if (TextUtils.isEmpty(newPasswordEditText.getText())) {
+                    newPasswordEditText.setError("You must enter your email.");
+                } else if (!isValidEmail(newPasswordEditText.getText())) {
+                    newPasswordEditText.setError("Please enter a valid email.");
+                } else {
+                    resetPasswordPresenter.resetPassword(
+                        newPasswordEditText.getText().toString().trim());
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
                 break;
             case R.id.btn_back:
                 finish();
@@ -39,6 +51,4 @@ public class ResetPasswordActivity extends BaseActivity implements View.OnClickL
 
         }
     }
-
-
 }
--- app/src/main/java/com/jzheadley/eat/utils/Utilities.java
+++ app/src/main/java/com/jzheadley/eat/utils/Utilities.java
@@ -1,7 +1,13 @@
 package com.jzheadley.eat.utils;
 
+import android.text.TextUtils;
+
 public class Utilities {
     public static double convertMetersToMiles(double meters) {
         return (meters / 1609.344);
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
